8992387edc701f373de36ebfa2da6d51df40abe5
lohithganni <116790357+lohithganni@users.noreply.github.com>
2024-07-12 00:23:17 +0530

    Update navbar.js

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
     <>
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Weather Tracker</a>
+    <a class="navbar-brand">Weather Tracker</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
